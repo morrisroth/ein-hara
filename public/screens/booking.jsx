8713405fefb51d553bbaca720a0ad3668a514450
morrisroth <morrisroth@example.com>
2026-05-24 02:29:33 +0300

    SEO: add static keyword content and structured data; update payment redirect to beit-ayin.com

--- public/screens/booking.jsx
+++ public/screens/booking.jsx
@@ -315,7 +315,7 @@ function StepPayment({ form, pkg, vp = {}, onPaymentDone }) {
   const popupRef = React.useRef(null);
 
   const BASE = 'https://www.matara.pro/nedarimplus/online/?mosad=7018027';
-  const REDIRECT = '&Redirect=' + encodeURIComponent('http://213.199.53.73:3001/?paid=1');
+  const REDIRECT = '&Redirect=' + encodeURIComponent('https://beit-ayin.com/?paid=1');
   const PKG_URLS = {
     segula:  BASE + '&OnlyNormal=1&Amount=149&AmountLock=1&Payment=1&PaymentLock=1' + REDIRECT,
     shmira:  BASE + '&NormalDefault=1&OnlyNormal=1&Amount=258&AmountLock=1&Payment=2&PaymentLock=1&Analytic=%D7%97%D7%91%D7%99%D7%9C%D7%AA%20%D7%A9%D7%9E%D7%99%D7%A8%D7%94%20-%20%D7%91%D7%99%D7%AA%20%D7%A2%D7%99%D7%9F' + REDIRECT,
